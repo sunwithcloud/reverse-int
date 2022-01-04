15f9e6fb269d0cfcf60e5de26bfdd61a12824f55
sunwithcloud <exentkill@gmail.com>
2022-01-04 19:26:50 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let newNumber = 0;
+
+    while ( n != 0 )
+    {
+        newNumber = (newNumber * 10) + (n % 10);
+        n = n / 10;
+        n = parseInt (n);
+    }
+
+    return Math.abs(newNumber);
 }
